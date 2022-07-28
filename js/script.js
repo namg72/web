d36a9a26bc1d77a4ce1d53692de5f00a56bc113f
namg72 <namg72@gmail.com>
2022-07-28 11:14:34 +0200

    Quienes somos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Enlaces de las tarjetas a las paginas
+
+let tarjetaLimpieza = document.getElementById("tarjetaLimpieza");
+tarjetaLimpieza.addEventListener("click", () => window.location.href = "/html/limpieza.html");
+
+let tarjetaLavanderia = document.getElementById("tarjetaLavanderia");
+tarjetaLavanderia.addEventListener("click", () => window.location.href = "/html/lavanderia.html");
+
+let tarjetaCelulosa = document.getElementById("tarjetaCelulosa");
+tarjetaCelulosa.addEventListener("click", () => window.location.href = "/html/celulosa.html");
