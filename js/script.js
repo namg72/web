6c4d9eb6e5e5c75c2cc830c57f9fa0a45f733fa6
namg72 <namg72@gmail.com>
2022-08-18 20:24:29 +0200

    isertada fecha actual

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-//Enlaces de las tarjetas a las paginas
+ // Función que nos devuelve la fecha actual con el dia de la semana
 
-let tarjetaLimpieza = document.getElementById("tarjetaPintura");
-tarjetaLimpieza.addEventListener("click", () => window.location.href = "/html/pinturas.html");
+ let fechaActual = () => {
+         let date = new Date;
+         const days = ["Domingo", "Lunes", "Marte", "Miercoles", "Jueves", "Viernes", "Sabado"]
+         const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+         let day = days[date.getDay()];
+         let month = months[date.getMonth()]
+         let fechaCompleta = `${day}, ${date.getDate()} de ${month} de ${date.getFullYear()}`
+         return fechaCompleta
+     }
+     //Insetamos la fecha en el header
+ let fecha = document.querySelector("header > p").textContent = fechaActual();
 
-let tarjetaLavanderia = document.getElementById("tarjetaLavanderia");
-tarjetaLavanderia.addEventListener("click", () => window.location.href = "/html/lavanderia.html");
 
-let tarjetaCelulosa = document.getElementById("tarjetaLimpieza");
-tarjetaCelulosa.addEventListener("click", () => window.location.href = "/html/limpieza.html");
+ //Enlaces Tarjetas index
 
+ let tarjetaLimpieza = document.getElementById("tarjetaPintura");
+ tarjetaLimpieza.addEventListener("click", () => window.location.href = "/html/pinturas.html");
+
+ let tarjetaLavanderia = document.getElementById("tarjetaLavanderia");
+ tarjetaLavanderia.addEventListener("click", () => window.location.href = "/html/lavanderia.html");
+
+ let tarjetaCelulosa = document.getElementById("tarjetaLimpieza");
+ tarjetaCelulosa.addEventListener("click", () => window.location.href = "/html/limpieza.html");
